9c3e96184f0afb7301aa8cedefd24364f372d269
christiandetratti <christian.detratti@edu.unife.it>
2022-10-20 17:31:00 +0200

    Completata procedura di login con profilo default

---
+++ src/main/java/isa/ProgettoEsame/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package isa.ProgettoEsame.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/login.html")
+    public String login() {
+      return "login.html";
+    }
+
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login.html";
+    }
+}
---
+++ src/main/java/isa/ProgettoEsame/Security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package isa.ProgettoEsame.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception 
+	{
+    	http
+			.csrf().disable()
+			.authorizeRequests().antMatchers("/login").permitAll()
+			.anyRequest().authenticated()
+		.and()
+        	.formLogin()
+        	.loginPage("/login.html")
+        	.failureUrl("/login-error.html")
+			.usernameParameter("userId")
+			.passwordParameter("pwd")
+			.permitAll()
+      	.and()
+       		.logout()
+        	.logoutSuccessUrl("/index.html");
+	}
+}
