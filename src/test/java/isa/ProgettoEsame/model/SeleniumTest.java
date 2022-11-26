8290b832cf2b9b711559cc04e03d824bb7774e1a
christiandetratti <christian.detratti@edu.unife.it>
2022-11-26 12:10:29 +0100

    Aggiunto test Selenium per admin (link)

--- src/test/java/isa/ProgettoEsame/model/SeleniumTest.java
+++ src/test/java/isa/ProgettoEsame/model/SeleniumTest.java
@@ -66,6 +66,7 @@ public class SeleniumTest {
         descr.sendKeys("Prova con Selenium-modified");
         driver.findElement(By.id("submit")).click();
         Thread.sleep(2000);
+
         /*Eliminazione della prenotazione*/
         driver.findElement(By.cssSelector("div>table>tbody>tr:last-of-type>td:last-of-type>a[class='btn btn-danger btn-sm mb-3']")).click();
         Thread.sleep(2000);
@@ -73,6 +74,40 @@ public class SeleniumTest {
         Thread.sleep(2000);
         alert.accept();
         Thread.sleep(4000);
+    }
+
+    @Test
+    public void testAdmin() throws InterruptedException {
+        /*Collegamento localhost*/
+        driver.get("http://localhost:8080/");
+
+        /*Procedura di Login*/
+        driver.findElement(By.name("userId")).sendKeys("detratti");
+        driver.findElement(By.name("pwd")).sendKeys("123");
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(2000);
+
+        /*Inserimento di un nuovo link*/
+    /*    driver.navigate().to("http://localhost:8080/link/add");
+        Thread.sleep(2000);
+        driver.findElement(By.name("destination")).sendKeys("Cracovia-Selenium");
+        driver.findElement(By.name("time")).sendKeys("6");
+        Thread.sleep(2000);
+        driver.findElement(By.name("submit")).click();
+        Thread.sleep(2000);
+         */
+
+        /*Modifca del collegamento relativo a Barcellona*/
+        driver.navigate().to("http://localhost:8080/link");
+        WebElement selectedRow = driver.findElement(By.xpath("//tr[td//text()[contains(.,'Barcellona')]]"));
+        selectedRow.findElement(By.cssSelector("td:last-of-type>a[class='btn btn-primary btn-sm mb-3']")).click();
+        Thread.sleep(2000);
+        WebElement timeLinked = driver.findElement(By.name("time"));
+        timeLinked.clear();
+        timeLinked.sendKeys("12");
+        Thread.sleep(2000);
+        driver.findElement(By.name("submit")).click();
+        Thread.sleep(2000);
     }
 
     @After
